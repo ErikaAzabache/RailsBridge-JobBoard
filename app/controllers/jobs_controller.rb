31d0a17a93f59b45d1106bda9da53c7d060b0eca
Erika Azabache <me@erikazabache.com>
2017-04-22 15:00:06 -0700

    adding update feature

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,23 @@
 class JobsController < ApplicationController
 	def index
 	end
+
 	def new
+		@job = Job.new
+	end
+
+	def create
+		Job.create(job_params)
+		redirect_to jobs_path
+	end
+
+	private
+
+	def job_params
+	  params.require(:job).permit(:title, :description)
+	end
+
+	def index
+		@jobs = Job.all
 	end
 end
